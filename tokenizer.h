47c3bf2b51ae9b054e57292ff44c5eea626c5509
Quizert <81437079+Quizert@users.noreply.github.com>
2024-01-22 23:00:26 +0300

    Create tokenizer.h

---
+++ tokenizer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <variant>
+#include <optional>
+#include <istream>
+
+struct SymbolToken {
+    std::string name;
+
+    bool operator==(const SymbolToken& other) const;
+};
+
+struct QuoteToken {
+    bool operator==(const QuoteToken&) const;
+};
+
+struct DotToken {
+    bool operator==(const DotToken&) const;
+};
+
+enum class BracketToken { OPEN, CLOSE };
+
+struct ConstantToken {
+    int value;
+
+    bool operator==(const ConstantToken& other) const;
+};
+
+using Token = std::variant<ConstantToken, BracketToken, SymbolToken, QuoteToken, DotToken>;
+
+class Tokenizer {
+public:
+    Tokenizer(std::istream* in);
+
+    bool IsEnd();
+
+    void Next();
+
+    Token GetToken();
+
+private:
+    std::istream* input_;
+    std::optional<Token> current_token_;
+    bool is_end_ = false;
+    void DeleteSpaces();
+    Token ReadToken();
+    ConstantToken GetConstant(bool is_negative);
+    BracketToken GetBracket();
+    SymbolToken GetSymbol();
+    QuoteToken GetQuote();
+    DotToken GetDot();
+};
